5fd4084310112316fc2bb4e49029ed857a388fb6
wassim ouanaou <wassim.ouanaou@hotmail.fr>
2023-06-30 15:35:22 -0700

    wassim

--- 0_functions.c
+++ 0_functions.c
@@ -45,4 +45,97 @@ int _print_char(va_list varg, char buff[], int flag,
 	return (write(1, &buff[0], 1));
 }
 
+/**
+ * _print_string -prints a string literal
+ * @varg: argument
+ * @buff: buffer
+ * @flag: flag
+ * @width: width
+ * @precision: preci
+ * @size: size
+ * Return: nb of printed
+ */
 
+int _print_string(va_list varg, char buff[], int flag,
+		int width, int precision, int size)
+{
+	int len, i;
+	char *arg;
+
+	len = 0;
+	arg = va_arg(varg, char*);
+	UNUSED(buff);
+	if (arg == NULL)
+	{
+		arg = "(null)";
+		if (precision >= 6)
+			arg = "      ";
+	}
+	while (arg[len] != '\0')
+		len++;
+	if (precision >= 0 && precision < len)
+		len = precision;
+	if (width > len)
+	{
+		if (flag & F_M)
+		{
+			write(1, &arg[0], len);
+			for (i = width - len; i > 0; i--)
+				write(1, " ", 1);
+			return (width);
+		}
+		else
+		{
+			for (i = width - len; i > 0; i--)
+				write(1, " ", 1);
+			write(1, &arg[0], len);
+			return (width);
+		}
+	}
+	UNUSED(flag);
+	UNUSED(precision);
+	UNUSED(size);
+	UNUSED(width);
+	UNUSED(buff);
+	UNUSED(varg);
+	return (write(1, arg, len));
+}
+
+
+/**
+ * _print_percent - print %%
+ * @varg: argments
+ * @buff: buffer
+ * @flag: flag
+ * @width: width
+ * @precision: preci
+ * @size: int
+ * Return: int
+ */
+
+
+int _print_percent(va_list varg, char buff[], int flag,
+		int width, int precision, int size)
+{
+	UNUSED(varg);
+	UNUSED(buff);
+	UNUSED(flag);
+	UNUSED(width);
+	UNUSED(precision);
+	UNUSED(size);
+	return (write(1, "%%", 1));
+}
+
+
+/**
+ * _print_int -prints int
+ * @varg: argments
+ * @buff: buffer
+ * @flag: flag
+ * @width: width
+ * @precision: preci
+ * @size: int
+ * Return: int
+ */
+
+int _print_int
